e006f2e1b42c3f0b3c7b4a4fa7e32e781506523d
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2017-06-24 18:05:28 -0400

    Add files via upload

--- sort_list.py
+++ sort_list.py
@@ -13,12 +13,66 @@ def fliptoright(array):
             array[i+1]=temp
     return array
 
-def select(array):
-    length = len(array)
-    for j in range(length):
-        array[:(length-j)]=fliptoright(array[:(length-j)])
+def bubble(array):
+    for j in range(len(array)):
+        fliptoright(array)
     return array
 
-array=[5,4,3,2]
 
-print select(array)
+#########################################
+
+def fliptoright2(array):
+    maxposition=0
+    maxnumber=array[0]
+    for i in range(len(array)):
+        if array[i]>maxnumber:
+            maxposition=i
+            maxnumber=array[i]
+    temp=array[-1]
+    array[-1]=maxnumber
+    array[maxposition]=temp
+    return array
+
+def select(array):
+    length = len(array)
+    for j in range(len(array)):
+        array[:(length-j)]=fliptoright2(array[:(length-j)])
+    return array
+
+def select2(array):
+    for j in range(len(array)-1):
+        maxposition=0
+        maxnumber=array[0]
+        for i in range(len(array)-j):
+            if array[i]>maxnumber:
+                maxposition=i
+                maxnumber=array[i]
+        temp=array[-1-j]
+        array[-1-j]=maxnumber
+        array[maxposition]=temp
+    return array
+
+
+
+import timeit
+iterative= timeit.timeit(
+"bubble([5,4,3,2,1])",
+"""
+def fliptoright(array):
+    for i in range(len(array)-1):
+        if array[i]>array[i+1]:
+            temp = array[i]
+            array[i]=array[i+1]
+            array[i+1]=temp
+    return array
+
+def bubble(array):
+    for j in range(len(array)):
+        fliptoright(array)
+    return array
+""",
+number=10000 ) # otherwise it takes 3 minutes
+
+print( "Iterative", iterative )
+
+        
