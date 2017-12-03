7de422426dcb916cd0899a38046ddb8ac6aa4ea0
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2017-12-03 12:44:41 -0500

    Add files via upload

---
+++ 83. Remove Duplicates from Sorted List.py
@@ -0,0 +1,27 @@
+﻿# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return None
+        if not head.next:
+            return head
+        current=head
+        prev=None
+
+        while current:
+            if prev and current.val==prev.val:
+                prev.next=current.next
+                current=current.next
+            else:
+                prev=current
+                current=current.next
+        return head
