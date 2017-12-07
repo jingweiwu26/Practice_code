c6a23cb014e8fbe82bb879906644fac1fac1a489
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2017-12-06 21:30:05 -0500

    Add files via upload

---
+++ 101. Symmetric Tree.py
@@ -0,0 +1,27 @@
+﻿# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.mirror(root.left, root.right)
+        
+    def mirror(self, left, right):
+        if not left and right:
+            return False
+        if not right and left:
+            return False
+        if not left and not right:
+            return True
+        if left.val != right.val:
+            return False
+        return self.mirror(left.left, right.right) and self.mirror(left.right, right.left)
