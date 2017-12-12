23d38247ced48ae2ea434b54e6cb589caf00b81a
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2017-12-11 22:43:39 -0500

    Add files via upload

---
+++ 110.Balanced Binary Tree.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 11 22:42:12 2017
+
+@author: Wu Jingwei
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        if abs(self.height(root.left)-self.height(root.right))<=1:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        else:
+            return False
+    
+    def height(self, root):
+        if not root:
+            return 0
+        return max(self.height(root.left), self.height(root.right))+1
