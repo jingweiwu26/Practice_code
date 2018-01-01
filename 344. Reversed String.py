59bfbd25d3b58591fc4b7e3479898a2f5739681e
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-01-01 11:59:58 -0500

    Add files via upload

---
+++ 344. Reversed String.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 01 11:59:17 2018
+
+@author: Wu Jingwei
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return "".join(reversed(s))
