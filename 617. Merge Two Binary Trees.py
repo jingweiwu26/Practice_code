5a7cd1aeffc23eff48438bdba655fcfd4d16d6a0
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-01-04 22:45:51 -0500

    Add files via upload

---
+++ 617. Merge Two Binary Trees.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 04 22:45:16 2018
+
+@author: Wu Jingwei
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if not t1 and not t2:
+            return
+        if not t1:
+            t1=TreeNode(0)
+        if not t2:
+            t2=TreeNode(0)
+        root = TreeNode(t1.val+t2.val)
+        root.left = self.mergeTrees(t1.left, t2.left)
+        root.right = self.mergeTrees(t1.right, t2.right)
+        return root
+        
