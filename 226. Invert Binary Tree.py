aebf9c2845be914dabe147aae135506aebca5d57
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-01-26 21:13:27 -0500

    Add files via upload

---
+++ 226. Invert Binary Tree.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 26 21:12:45 2018
+
+@author: Wu Jingwei
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if not root:
+            return
+        root.left, root.right = root.right,root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
