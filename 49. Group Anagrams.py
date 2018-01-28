dbd6eaab6f2d436ad8ff72b76f04403bedc25395
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-01-28 13:47:33 -0500

    Add files via upload

---
+++ 49. Group Anagrams.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 13:46:32 2018
+
+@author: Wu Jingwei
+"""
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dic={}
+        for i in strs:
+            if "".join(sorted(i)) not in dic:
+                dic["".join(sorted(i))] = [i]
+            else:
+                dic["".join(sorted(i))].append(i)
+        res=[]
+        for key, value in dic.iteritems():
+            res.append(value)
+        return res
