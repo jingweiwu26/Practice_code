b3e499eb6322c71e004c40aaeff20337a319c808
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-03-23 22:40:15 -0400

    Add files via upload

---
+++ 199. Binary Tree Right Side View.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 22:39:44 2018
+
+@author: Wu Jingwei
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+
+        current = [root]
+        res=[]
+        while current:
+            next = []
+            for node in current:
+                if node.left:
+                    next.append(node.left)
+                if node.right:
+                    next.append(node.right)
+            res.append(node.val)
+            current = next
+        return res
