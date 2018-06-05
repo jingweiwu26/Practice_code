46a56521dbf1e0a2914b198b9a7ec327de651066
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-06-04 22:31:16 -0400

    Update 50. pow(x,n).py

--- 50. pow(x,n).py
+++ 50. pow(x,n).py
@@ -22,5 +22,25 @@ class Solution(object):
             return self.myPow(x, n/2)**2
         if n%2==1:
             return self.myPow(x,n-1)*x
-        
-        
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if x == 0:
+            return 1
+        if n < 0 :
+            x = 1/x
+            n *= -1
+        if n == 1:
+            return x
+        if n == 0:
+            return 1
+        while n > 1:
+            if n % 2 == 0:
+                return self.myPow(x, n/2) ** 2
+            if n % 2 == 1:
+                return self.myPow(x, n-1) * x
