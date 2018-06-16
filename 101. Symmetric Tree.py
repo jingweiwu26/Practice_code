6a859b2a8b2ea1c3a9c9050aa2a066710659db75
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-06-15 22:18:16 -0400

    Update 101. Symmetric Tree.py

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -25,3 +25,24 @@ class Solution(object):
         if left.val != right.val:
             return False
         return self.mirror(left.left, right.right) and self.mirror(left.right, right.left)
+
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        return self.helper(root.left, root.right)
+    
+    def helper(self, l, r):
+        if not l and not r:
+            return True
+        if l and not r:
+            return False
+        if r and not l:
+            return False
+        if l.val != r.val:
+            return False
+        return self.helper(l.left, r.right) and self.helper(l.right, r.left)
