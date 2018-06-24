91409b9caf82cd301322f02aa0852fcd90dc47c6
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2018-06-23 23:20:06 -0400

    Update 206. Reverse Linked List.py

--- 206. Reverse Linked List.py
+++ 206. Reverse Linked List.py
@@ -22,4 +22,18 @@ class Solution(object):
             current = _next
 
         return prev
-        
+    
+  class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        current = head
+        prev = None
+        while current:
+            _ = current.next
+            current.next = prev
+            prev = current
+            current = _
+        return prev
