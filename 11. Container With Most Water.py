efd30d89dc19129ff653f688cd0cff64b7ffb327
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2020-12-29 10:58:01 -0500

    Update 11. Container With Most Water.py

--- 11. Container With Most Water.py
+++ 11. Container With Most Water.py
@@ -27,3 +27,26 @@ class Solution(object):
                     max_area = area
                 right -= 1
         return max_area
+
+    class Solution:
+        def maxArea(self, height: List[int]) -> int:
+            combinations = [(height_i, height_j, abs(idx_i-idx_j)) 
+                            for idx_i, height_i in enumerate(height) 
+                            for idx_j, height_j in enumerate(height)]
+            max_area = max(combinations, key=lambda x: min(x[0], x[1]) * x[2])
+            return min(max_area[0], max_area[1]) * max_area[2]
+    
+    class Solution:
+        def maxArea(self, height: List[int]) -> int:
+            idx_l = 0
+            idx_r = len(height) - 1
+            max_area = 0
+            while idx_l != idx_r:
+                max_area = max(max_area, min(height[idx_l], height[idx_r]) 
+                               * abs(idx_l - idx_r))
+                if height[idx_l] <= height[idx_r]:
+                    idx_l += 1
+                else:
+                    idx_r -= 1 
+            return max_area
+        
