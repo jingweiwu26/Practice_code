36a21fff8bd2ebc84375b87fbe3d7a83c6f5d1b3
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2021-02-14 22:43:35 -0500

    Update 22. Generate Parentheses.py

--- 22. Generate Parentheses.py
+++ 22. Generate Parentheses.py
@@ -48,3 +48,17 @@ class Solution:
                 gen(o, c-1, s+')')
         gen(n, n, '')
         return res
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def backtrack(s, l, r):
+            if len(s) == 2*n:
+                res.append(s)
+            if l < n:
+                backtrack(s + '(', l+1, r)
+            if r < l:
+                backtrack(s + ')', l, r+1)
+        backtrack('', 0, 0)
+        return res
