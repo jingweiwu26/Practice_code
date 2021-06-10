8827565decc789540c8bc746b1347f7bc5a491b8
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2021-06-10 09:36:57 -0400

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -14,3 +14,29 @@ class Solution(object):
         from itertools import permutations
         p=permutations(nums)
         return [list(i) for i in p]
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(path, select):
+            if not select:
+                res.append(path[:])
+                return
+            for i in range(len(select)):
+                path.append(select[i])
+                backtrack(path, select[:i]+ select[i+1:])
+                path.pop()
+        backtrack([], nums)
+        return res
+    
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(nums, tmp):
+            if not nums:
+                res.append(tmp)
+                return
+            for i in range(len(nums)):
+                backtrack(nums[:i] + nums[i+1:], tmp + [nums[i]])
+        backtrack(nums, [])
+        return res
