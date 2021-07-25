68e57b960443832af729c34fbafe08407f0728ba
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2021-07-25 15:53:34 -0400

    Update 48. Rotate Image.py

--- 48. Rotate Image.py
+++ 48. Rotate Image.py
@@ -17,3 +17,24 @@ class Solution(object):
                 matrix[i][j] , matrix[j][i] = matrix[j][i], matrix[i][j]
         for i in matrix:
             i.reverse()
+ 
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        step_1 = [list(i)for i in zip(*matrix)]
+        print(step_1)
+        step_m = [i[::-1] for i in step_1]
+        for row in range(len(matrix)):
+            for col in range(len(matrix)):
+                matrix[row][col] = step_m[row][col]
+        return matrix
+    
+        # 1 2 3
+        # 4 5 6
+        # 7 8 9
+        
+        # 1 4 7
+        # 2 5 8
+        # 3 6 9 
