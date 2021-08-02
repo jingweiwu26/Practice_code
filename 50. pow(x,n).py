37d53bf26a2447f99abab7037f17a48401c797da
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2021-08-01 21:41:52 -0400

    Update 50. pow(x,n).py

--- 50. pow(x,n).py
+++ 50. pow(x,n).py
@@ -44,3 +44,19 @@ class Solution(object):
                 return self.myPow(x, n/2) ** 2
             if n % 2 == 1:
                 return self.myPow(x, n-1) * x
+            
+ class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        elif n == 1:
+            return x
+        elif n < 0:
+            x = 1/x
+            n *= -1
+            return self.myPow(x, n)
+        #   return 1 / self.myPow(x, -n) -- causes over flow
+        elif n % 2 == 0:
+            return self.myPow(x, n / 2)**2
+        elif n % 2 == 1:
+            return x * self.myPow(x, n - 1)
