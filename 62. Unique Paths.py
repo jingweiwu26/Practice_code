945355470c85e32c4672c0eda1ab45d3819bd332
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2021-10-26 11:59:52 -0400

    Update 62. Unique Paths.py

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -25,3 +25,19 @@ class Solution(object):
                 res[i][j]=res[i-1][j] + res[i][j-1]
                 print res[i-1][j], res[i][j-1], res[i][j]
         return res[i][j]
+    
+    
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * m for i in range(n)]
+        dp[0] = [1] * m
+        for i in range(n):
+            dp[i][0] = 1
+    
+        for i in range(1,n):
+            for j in range(1,m):
+                if i!=0 or j!=0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+ 
+        return dp[-1][-1]
+                
