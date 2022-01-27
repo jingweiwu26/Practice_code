ef4a7a77e601bed905ccbd0ef5c82097c1063dc5
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2022-01-26 21:09:07 -0500

    Update 122. Best Time to Buy and Sell Stock II.py

--- 122. Best Time to Buy and Sell Stock II.py
+++ 122. Best Time to Buy and Sell Stock II.py
@@ -17,3 +17,13 @@ class Solution(object):
             if prices[i+1]>prices[i]:
                 p+= (prices[i+1]-prices[i])
         return p
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        dp0 = 0 # state when not holding
+        dp1 = -prices[0] # state when holding
+        for p in prices[1:]:
+            dp0 = max(dp0, dp1 + p)
+            dp1 = max(dp1, dp0 - p)
+            
+        return dp0
