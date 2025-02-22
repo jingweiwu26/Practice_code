33c206ad2b754ae09add6147f7a58925b453cc1c
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2025-02-22 15:09:41 -0800

    Update 110.Balanced Binary Tree.py

--- 110.Balanced Binary Tree.py
+++ 110.Balanced Binary Tree.py
@@ -29,3 +29,26 @@ class Solution(object):
         if not root:
             return 0
         return max(self.height(root.left), self.height(root.right))+1
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def post_order(n: Optional[TreeNode]) -> int:
+            if not n: return 0
+
+            l = post_order(n.left)
+            r = post_order(n.right)
+
+            if abs(l-r) > 1: return -1
+            if l == -1 or r == -1: return -1
+
+            return max(l, r) +1
+        
+        res = post_order(root)
+        return res != -1
