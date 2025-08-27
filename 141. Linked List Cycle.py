2ad879b8d8d85832ec5a8ee198d8f1dbf617a557
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2025-08-26 19:22:48 -0700

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -29,3 +29,20 @@ class Solution(object):
             first=first.next
             second=second.next.next
         return False
+        
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        f = s = head
+        while f and f.next:
+            f=f.next.next
+            s=s.next
+            if s is f:
+                return True
+        return False
