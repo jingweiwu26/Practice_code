59fe045ea7fbc54de366bae892af98ae556b572e
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2025-08-27 20:27:08 -0700

    Update 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -27,3 +27,26 @@ class Solution(object):
             mapping[id(current)]=current.val
             current=current.next
         return None
+        
+        
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        f = s = head
+        while f and f.next:
+            f = f.next.next
+            s = s.next
+            
+            if f == s:
+                s = head
+                while f != s :
+                    s = s.next
+                    f = f.next
+                return f
+        
+        return None
