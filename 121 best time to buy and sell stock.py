7cbbeebfea2003186129e4091d728f600f77f71c
jingweiwu26 <jingwei.wu@baruchmail.cuny.edu>
2025-11-24 21:57:52 -0500

    Update 121 best time to buy and sell stock.py

--- 121 best time to buy and sell stock.py
+++ 121 best time to buy and sell stock.py
@@ -37,3 +37,19 @@ class Solution:
             max_profit = max(max_profit, price - lowest_price)
             
         return max_profit
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        dp = [[0,0] for j in range(n)]
+        
+        dp[0][0] = 0            # no stock
+        dp[0][1] = -prices[0] 
+        
+        
+        for i in range(1,n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+            dp[i][1] = max(dp[i-1][1], -prices[i])
+        
+        return dp[n-1][0]
